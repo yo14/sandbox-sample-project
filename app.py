a5fc82063bc657e72e45c6c65df05b9cc5a40f15
Yolla Ifliandry <ifliandry11@gmail.com>
2020-06-20 16:37:28 +0700

    Using Bootstrap4 library

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap   # step 01 import library
 
 app = Flask(__name__)
+Bootstrap(app)     # step 02 to activate boostrap
 
 @app.route('/')
 def index():
@@ -10,8 +12,14 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/css')
+def css():
+    return render_template('css.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
 
 
