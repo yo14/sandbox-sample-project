50674c5f63c247fe465e38d58711c550920fb4fc
Yolla Ifliandry <ifliandry11@gmail.com>
2020-06-20 09:45:43 +0700

    Redirect page using function url_for() and redirect()

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return redirect(url_for('about'))     # about is def name, NOT route name
 
 @app.route('/about')
 def about():
