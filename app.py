c65b5ed1bba2f033282480bb20c1f429ff1e0696
yo14 <ifliandry11@gmail.com>
2020-06-23 10:09:38 +0700

    Start using mysql: Input data

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap   # step 01 import library
+from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
-Bootstrap(app)     # step 02 to activate boostrap
+Bootstrap(app)
+
+# Configure db
+# db = yaml.load(open('db.yaml'))  yaml.load sudah deprecated
+db = yaml.safe_load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO user VALUES(%s)",['Mike'])  # 'Mike' should in list form
+    mysql.connection.commit()
     return render_template('index.html')
 
 @app.route('/about')
