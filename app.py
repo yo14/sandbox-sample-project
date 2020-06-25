30c56d385a1b190c0dd18a06a6e3ed323b62be3f
yo14 <ifliandry11@gmail.com>
2020-06-25 16:36:31 +0700

    first commit for submit-form project

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap
+from flask import Flask
 from flask_mysqldb import MySQL
 import yaml
 
 app = Flask(__name__)
-Bootstrap(app)
 
 # Configure db
 # db = yaml.load(open('db.yaml'))  yaml.load has deprecated
@@ -15,36 +13,7 @@ app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 mysql = MySQL(app)
 
-@app.route('/')
-def index():
-    cur = mysql.connection.cursor()
-    # cur.execute("INSERT INTO user VALUES(%s)",['Mike'])
-    # mysql.connection.commit()
-    result_value = cur.execute("SELECT * FROM user")
-    if result_value > 0:
-        users = cur.fetchall()
-        print(users)  # you can see the result inside Terminal, after refresh the browser
-    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/css')
-def css():
-    return render_template('css.html')
-
-@app.route('/testing', methods=['GET','POST'])   # arrange method post
-def sendme():
-    # Perhatikan bagian ini
-    if request.method == 'POST':
-        # return 'Successfully registered'
-        return request.form['password']       # get data from form input
-    return render_template('testingpage.html')
-
-@app.errorhandler(404)
-def page_in_dream(e):
-    return 'Please change your mind, find another request'
 
 if __name__ == '__main__':
     app.run(debug=True)
