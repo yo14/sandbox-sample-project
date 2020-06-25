d0c4bb07edefb7f0b4b78b00a3498d479b2b9eb2
yo14 <ifliandry11@gmail.com>
2020-06-26 03:17:05 +0700

    Make encryption for sensitive data

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 
 from flask import Flask, render_template, request, session
 from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
 import yaml
+
 
 app = Flask(__name__)
 
@@ -25,6 +27,7 @@ def index():
         name = form['name']
         age  = form['age']
         cur = mysql.connection.cursor()
+        name = generate_password_hash(name)
         cur.execute("INSERT INTO employee(name, age) VALUES(%s, %s)", (name, age))
         mysql.connection.commit()
     return render_template('index.html')
@@ -35,8 +38,9 @@ def employees():
     result_value = cur.execute("SELECT * FROM employee")
     if result_value > 0:
         employees = cur.fetchall()
-        session['username'] = employees[0]['name']
-        return render_template('employees.html', employees=employees)
+        return str(check_password_hash(employees[3]['name'], 'yo14_dev'))  # just only for testing purpose
+        # session['username'] = employees[0]['name']
+        # return render_template('employees.html', employees=employees)
 
 
 if __name__ == '__main__':
