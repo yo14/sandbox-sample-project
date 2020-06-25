e9cb3db072a8b38a4af103f3a60cab0143dc4e54
yo14 <ifliandry11@gmail.com>
2020-06-25 17:34:37 +0700

    make Create and Read table data

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # sandbox-sample-project
-Place to trial and error new skill
+Place to trial and error some skills
 
 note for:<br> <code>pip install flask-mysqldb</code> <br>
 If having error like:  <br><code>ERROR: Command errored out with exit status 1: python setup.py egg_info Check the logs for full command output.</code><br><br>Solution: <br><code>sudo apt-get install python-pip python-dev libmysqlclient-dev</code> <br> outside of virtual environtment.
+<br></br>
+<br>Make database and a table for submit-form project<br>
+<pre>
+mysql> CREATE DATABASE employee_data;
+Query OK, 1 row affected (0,00 sec)
+
+mysql> USE employee_data;
+Database changed
+mysql> CREATE TABLE employee(name varchar(20), age integer);
+Query OK, 0 rows affected (0,55 sec)
+
+mysql> SELECT * FROM employee;
+Empty set (0,00 sec)
+</pre>
+
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 import yaml
 
@@ -11,8 +11,27 @@ app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'         # it makes the data can call as a object variable.fieldname
 mysql = MySQL(app)
 
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        form = request.form
+        name = form['name']
+        age  = form['age']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO employee(name, age) VALUES(%s, %s)", (name, age))
+        mysql.connection.commit()
+    return render_template('index.html')
+
+@app.route('/employees')
+def employees():
+    cur = mysql.connection.cursor()
+    result_value = cur.execute("SELECT * FROM employee")
+    if result_value > 0:
+        employees = cur.fetchall()
+        return render_template('employees.html', employees=employees)
 
 
 if __name__ == '__main__':
