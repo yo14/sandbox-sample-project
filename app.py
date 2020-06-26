dfb81284165e73912416e2bc4095c4e2f6b1a054
yo14 <ifliandry11@gmail.com>
2020-06-26 14:15:38 +0700

    Add Navigation menu on top project

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
 
 from flask import Flask, render_template, request, session
+from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 import yaml
 
 
 app = Flask(__name__)
+Bootstrap(app)
 
 # Configure db
 # db = yaml.load(open('db.yaml'))  yaml.load has deprecated
@@ -38,10 +40,9 @@ def employees():
     result_value = cur.execute("SELECT * FROM employee")
     if result_value > 0:
         employees = cur.fetchall()
-        return str(check_password_hash(employees[3]['name'], 'yo14_dev'))  # just only for testing purpose
-        # session['username'] = employees[0]['name']
-        # return render_template('employees.html', employees=employees)
-
+        # return str(check_password_hash(employees[3]['name'], 'yo14_dev'))  # just only for testing purpose
+        session['username'] = employees[0]['name']
+        return render_template('employees.html', employees=employees)
 
 if __name__ == '__main__':
     app.run(debug=True)
